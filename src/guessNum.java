d6440a5f70783939f538d5927183adb45688a647
sirius gurban <failoglu96@gmail.com>
2019-10-18 15:43:57 +0400

    GuessNumWork

---
+++ src/guessNum.java
@@ -0,0 +1,44 @@
+package src;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNum {
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Enter your name: ");
+        Scanner inputName = new Scanner(System.in);
+        name = inputName.nextLine();
+        Random rand = new Random();
+        int userNum = rand.nextInt(100);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Let the game begin: ");
+        int guessNum;
+        boolean win = false;
+
+
+        while (win == false){
+
+
+            guessNum = input.nextInt();
+
+
+
+            if(guessNum == userNum) {
+                win = true;
+            }
+            else if(guessNum<userNum){
+                System.out.println("Your number is too small. Please, try again.");
+            }
+            else if(guessNum>userNum){
+                System.out.println("Your number is too big. Please, try again.");
+            }
+        }
+
+
+        System.out.println("The number was " + userNum);
+        System.out.println("Congratulations, " + name + "!");
+
+
+    }
+}
