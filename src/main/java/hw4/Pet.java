1346576cb2639af44425d98002e740775968b6d2
siriusgurban <56581897+siriusgurban@users.noreply.github.com>
2019-10-25 22:23:23 +0400

    Update Pet.java

--- src/main/java/hw4/Pet.java
+++ src/main/java/hw4/Pet.java
@@ -2,15 +2,13 @@ package hw4;
 
 public class Pet {
 
-    String species;
-    String nickname;
-    int age;
-    int trickLevel;
-    String habits;
+    public String species;
+    public String nickname;
+    public int age;
+    public int trickLevel;
+    public String habits;
 
-
-    public Pet(String species, String nickname, int age, int trickLevel, String habits)
-    {
+    public Pet(String species, String nickname, int age, int trickLevel, String habits) {
         this.species = species;
         this.nickname = nickname;
         this.age = age;
@@ -18,53 +16,40 @@ public class Pet {
         this.habits = habits;
     }
 
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
 
-    public void getEat()
-    {
-        System.out.println("I am eating");
+    public Pet() {
 
     }
 
 
-    public void getRespond()
-    {
+
+    public void getEat() {
+        System.out.println("I am eating");
+    }
+
+
+    public void getRespond() {
         System.out.println("Hello, owner. I am - " + species + ". " + "I miss you!");
     }
 
 
-    public void getFoul()
-    {
+    public void getFoul() {
         System.out.println("I need to cover it up");
     }
 
 
-    public String getSpecies() {
-        return species;
-    }
 
-    public String getHabits() {
-        return habits;
-    }
-
-    public int getTrickLevel() {
-        return trickLevel;
-    }
-
-    public int getAge() {
-        return age;
+    public String toString()
+    {
+        return species + "{"+ "nickname"+ "="+ nickname  + ", " + "age" + "=" + age + ", "+ "trickLevel"+ "=" + trickLevel + ", "+ "habits" + "=["+ habits + "]}";
     }
 
 
-    public String getNickname(){
-        return nickname;
-    }
 
-    @Override
-    public String toString(){
-        return(this.getSpecies() + "{" + "nickname=" + "'" + this.getNickname()+"'" + ", " + "age" + "=" + this.getAge()+ ", " + "trickLevel" + "="  + this.getTrickLevel() + ", " + "habits" + "[" + this.getHabits() + "]"+ "}");
-    };
-
-//    dog{nickname='Rock', age=5, trickLevel=75, habits=[eat, drink, sleep]}
 }
 
 
