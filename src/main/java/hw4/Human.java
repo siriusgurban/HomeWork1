4104567699da48e5e3a0d5cbeea61b2ee85ede1c
siriusgurban <56581897+siriusgurban@users.noreply.github.com>
2019-10-25 22:25:49 +0400

    Update Human.java

--- src/main/java/hw4/Human.java
+++ src/main/java/hw4/Human.java
@@ -1,43 +1,62 @@
 package hw4;
 
-public class Human {
+public class Human extends Pet{
+    public String name;
+    public String surname;
+    public int iq;
+    public int year;
+    public String pet;
+    public String mother;
+    public String father;
 
-    String name;
-    String surname;
-    int year;
-    int iq;
-    String mother;
-    String father;
-    String pet;
+    public void getGreetPet() {
+        System.out.println("Hello, " + nickname);
+    }
+
+
+    public void getDescribePet() {
+        System.out.println("I have a " + species + ", he is " + age + " years old, he is [very sly]>50/[almost not sly]<50");
+    }
 
 
 
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+    public Human(String name, String surname, int year, String mother, String father) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.mother = mother;
+        this.father = father;
+    }
 
     public Human(String name, String surname, int year, int iq, String mother, String father, String pet) {
         this.name = name;
         this.surname = surname;
-        this.year = year;
         this.iq = iq;
+        this.year = year;
         this.mother = mother;
         this.father = father;
         this.pet = pet;
     }
 
-
-    public void getGreetPet()
-    {
-        System.out.println("Hello, + " );
+    public Human() {
 
     }
 
-
-    public void getDescribePet()
+    public String toString()
     {
-        System.out.println("I have a " + Pet.class.toString() +" he is " + "age");
+        return "Human" + "{"+ "name"+ "=" + name  + ", " + "surname" + "=" + surname + ", " + "year"+ "=" + year + ", "
+                + "iq"+ "=" + iq + ", " + "mother"+ "=" + mother + ", " + "father"+ "=" + father + ", ";
     }
 
 
 
+    //species + "{"+ "nickname"+ "="+ nickname  + ", " + "age" + "=" + age + ", "+ "trickLevel"+ "=" + trickLevel + ", "+ "habits" + "=["+ habits + "]}"
 
 }
 
