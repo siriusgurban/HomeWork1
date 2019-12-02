43b0ba84221d99019552fe1fa0eef3a8a109da89
sirius gurban <failoglu96@gmail.com>
2019-12-02 22:52:21 +0400

    ahaa

--- src/main/java/hw7/Main.java
+++ src/main/java/hw7/Main.java
@@ -9,16 +9,16 @@ public class Main {
     public static void main(String[] args) {
 //        Pet dog = new Pet(Species.DOG, "Rock", 5, 75, new String[]{"eat", "sleep"});
 //        Human grandfather = new Human();
-        Human father  = new Human("Donatello","Cell", 1960) ;
-        Human mother = new Human("April","Cell", 1970);
-        Human child1 = new Human("Michelangelo","Cell", 1994, 90, new String[][]{{DayOfWeek.SUNDAY.name(), "Task_1"}, {DayOfWeek.FRIDAY.name(), "Task_2"}});
+//        Human father  = new Human("Donatello","Cell", 1960) ;
+//        Human mother = new Human("April","Cell", 1970);
+//        Human child1 = new Human("Michelangelo","Cell", 1994, 90, new String[][]{{DayOfWeek.SUNDAY.name(), "Task_1"}, {DayOfWeek.FRIDAY.name(), "Task_2"}});
 
 
 
-        System.out.println(child1);
-        System.out.println(father);
-        Family family = new Family(father, mother);
-        family.addChild(child1);
-        System.out.println(Arrays.toString(family.getChildren()));
+//        System.out.println(child1);
+//        System.out.println(father);
+//        Family family = new Family(father, mother);
+//        family.addChild(child1);
+//        System.out.println(Arrays.toString(family.getChildren()));
     }
 }
